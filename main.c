e2e52c6af288c16f41be867db7975d402bc9976a
VictoriaTejera <victoriatejera15@gmail.com>
2019-05-21 19:40:07 -0300

    Se corrige cd para que funcione 'cd -'

--- main.c
+++ main.c
@@ -25,17 +25,30 @@
 
 
 int status = SIN_STATUS;
+char last_dir[MAX];
 
 int builtin_cd(int argc, char **argv){
-	int salida;
-
+    int salida;
+    char temp[MAX];
+    
     if(argv[1] == NULL){
+
+	getcwd(last_dir, sizeof(last_dir));
         salida = chdir("/home");
+    
+    }else if(strcmp(argv[1],"-") == 0){
+        
+        getcwd(temp, sizeof(temp));
+        salida = chdir(last_dir);
+        strcpy(last_dir, temp);
+    
     }else{
+
+        getcwd(last_dir,sizeof(last_dir));
         if( (salida = chdir(argv[1])) != 0)
-			perror("cd failed");
-    } 
-	        
+            perror("cd failed");
+    }
+
     return salida;
 }
 
